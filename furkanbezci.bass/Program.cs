c0cfe5430820694d6d037329742308995c1e2e8c
Necmettin <necmettin.cimen@alpata.com>
2020-10-14 20:31:11 +0300

    Add project files.

---
+++ furkanbezci.bass/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace furkanbezci.bass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("furkanbezci.bass!");
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine(i);
+            }
+            for (int i = 100; i >= 1; --i)
+            {
+                Console.WriteLine(i);
+            }
+            
+            Console.Write("Input n :");
+            var nstr = Console.ReadLine(); //kullanıcıdan değer oku
+             
+            if(!int.TryParse(nstr, out int n))
+            {
+                Console.WriteLine("Lütfen int bir değer giriniz");
+            }
+            else
+            {
+                Console.WriteLine("Sum: " + Sum(n));
+                Console.WriteLine("Sum_A: " + Sum_A(n));
+            }
+        }
+
+        public static int Sum(int n)
+        {
+            return n*(n+1) / 2;
+        }
+        public static int Sum_A(int n)
+        {
+            int result = 0;
+            for (int i = 0; i <= n; i++)
+            {
+                result += i;
+            }
+            return result;
+        }
+    }
+}
